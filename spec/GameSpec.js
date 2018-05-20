cdd3fe585a904bd913d9502425ed3676762d7ad1
William Powell <william.powell92@gmail.com>
2018-05-20 18:55:51 +0100

    game can calculate gutter and open game scores

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -11,6 +11,10 @@ describe('Game', function () {
     expect(game.frames).toEqual([])
   })
 
+  it('is created with a score of 0', function () {
+    expect(game.score).toEqual(0)
+  })
+
   describe('addFrame', function () {
     it('adds a frame to the frames array', function () {
       game.addFrame(frame)
@@ -24,7 +28,27 @@ describe('Game', function () {
     })
   })
 
-  // it('scores a gutter game correctly', function () {
-  //   game.frames
-  // })
+  describe('calculateCurrentScore', function () {
+    it('scores a gutter game correctly', function () {
+      for (i = 0; i < 10; i++) {
+        var frame = new Frame()
+        frame.addBowl({ count: 0 })
+        frame.addBowl({ count: 0 })
+        game.addFrame(frame)
+      }
+      game.calculateCurrentScore()
+      expect(game.score).toEqual(0)
+    })
+
+    it('scores an open game correctly', function () {
+      for (i = 0; i < 10; i++) {
+        var frame = new Frame()
+        frame.addBowl({ count: 4 })
+        frame.addBowl({ count: 4 })
+        game.addFrame(frame)
+      }
+      game.calculateCurrentScore()
+      expect(game.score).toEqual(80)
+    })
+  })
 })
--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,14 @@
 function Game () {
   this.frames = []
+  this.score = 0
 }
 
 Game.prototype.addFrame = function (frame) {
   this.frames.push(frame)
+}
+
+Game.prototype.calculateCurrentScore = function () {
+  this.frames.forEach(function (frame) {
+    this.score += frame.score()
+  }, this)
 }
